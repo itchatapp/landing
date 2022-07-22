f17adca0b54e575f90bcda435186733ec583e799
MohaDev69 <MohaDev69.dev@outlook.com>
2022-07-23 00:01:07 +0300

    feat: add shadow , navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import  Navbar  from "./components/navbar";
+import { MantineProvider } from "@mantine/core";
+const links = [{
+link: "/" , label: "Home" , key: "home"
+}, {
+link: "/developers" , label: "Developers" , key: "developers"
+},{
+link: "https://github.com/itchatapp/" , label: "GitHub" , key: "github"
+}, {
+link: "/contact" , label: "Contact" , key: "Contact"
+}]
 
-function App() {
-
+function App(){
   return (
     <>
+  <MantineProvider theme={{ colorScheme: 'dark' }} >
+    <Navbar links={links}/>
+    <div className="shadow"></div>
+    </MantineProvider>
     </>
+
   )
 }
 
